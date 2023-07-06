aed3a036896475b70d6977bfeef3ed8459406483
LuisPSilva98 <108488545+LuisPSilva98@users.noreply.github.com>
2023-07-06 18:32:47 -0300

    Criando migrações e conectando com o banco d dados

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -11,43 +12,24 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+       private readonly DataContext _context;
 
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento() {
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                Lote = "1ª",
-                ImageURL = "Foto1.png"
-            },
-            new Evento() {
-                EventoId = 2,
-                Tema = "Angular",
-                Local = "São Paulo",
-                QtdPessoas = 300,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                Lote = "2ª",
-                ImageURL = "Foto2.png"
-            }
-        };
-
-        public EventoController(ILogger<EventoController> logger)
+        public EventoController(DataContext context)
         {
-
+           _context = context;
         }
+
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(Evento => Evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(Evento => Evento.EventoId == id);
         }
 
 
